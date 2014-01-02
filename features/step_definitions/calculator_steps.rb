2a9a2712e33a0b3c7834114a96e291889b9882b0
Erran Carey <me@errancarey.com>
2014-01-02 00:54:46 -0600

    Initial commit (add i18n en cuke examples)

---
+++ features/step_definitions/calculator_steps.rb
@@ -0,0 +1,21 @@
+require 'rspec/expectations'
+require_relative '../../lib/calculator'
+
+Before do
+  @calc = Calculator.new
+end
+
+After do
+end
+
+Given /I have entered (\d+) into the calculator/ do |n|
+  @calc.push n.to_i
+end
+
+When /I press (\w+)/ do |op|
+  @result = @calc.send op
+end
+
+Then /the result should be (.*) on the screen/ do |result|
+  @result.should == result.to_f
+end
